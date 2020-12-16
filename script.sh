307faa9c55ce2dec58b7229fdde1fe5f0dedf95e
imav9 <72993287+imav9@users.noreply.github.com>
2020-12-16 21:28:36 +0100

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash/
+
+mv ~/.config/kitty/ ~/.config/kitty/_
+mv ~/.config/openbox ~/.config/openbox_
+mv ~/.config/rofi ~/.config/rofi_/
+mv ~/.config/picom/ ~/.config/picom.conf_
+mv ~/.config/zathura/ ~/.config/zathura_/
+mv ~/.config/fish/ ~/.config/fish_/
+mv ~/.config/berry/ ~/.config/berry_/
+mv ~/.config/sxhkd/ ~/.config/sxhkd_
+
+mv kitty/ ~/.config/kitty/
+mv openbox ~/.config/openbox/
+mv rofi ~/.config/rofi/
+mv picom.conf ~/.config/picom.conf
+mv zathura/ ~/.config/zathura/
+mv fish/ ~/.config/fish/
+mv berry/ ~/.config/berry/
+mv sxhkd/ ~/.config/sxhkd
+
+mv wallpaper.jpg ~/Pulpit/.wallpaper.jpg
